cfd8e2cea294c08a45f707035610efcd18a5272e
Joaquin Blanco <joaquinbf@live.com>
2020-10-29 19:37:18 +0000

    create route loggin

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,11 +1,16 @@
 const index_route = {}
 
+/* render de rutas principales */
 index_route.render_body = (req, res) => {
 	res.render('body');
 }
 
 index_route.render_entrevista_dinamica = (req, res) => {
 	res.render('entrevista_dinamica');
+}
+
+index_route.render_iniciar_sesion = (req, res) => {
+	res.render('iniciar_sesion');
 }
 
 module.exports = index_route;
--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const {render_body, render_entrevista_dinamica} = require('../controllers/index.controller')
+const {render_body, render_entrevista_dinamica, render_iniciar_sesion} = require('../controllers/index.controller')
 
+/*rutas principales */
 router.get('/', render_body);
 
 router.get('/entrevista-dinamica', render_entrevista_dinamica);
 
+router.get('/iniciar-sesion', render_iniciar_sesion);
 
 module.exports = router;
