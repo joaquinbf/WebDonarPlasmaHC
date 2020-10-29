a439070c38fd414ac908d81e1c2fcb334318b37d
rocio121 <rcanaviri@etec.uba.ar>
2020-10-29 14:22:16 +0000

    agregamos boton quiero donar y la ruta entrevista dinamica

--- src/server.js
+++ src/server.js
@@ -4,27 +4,30 @@ const exphbs = require("express-handlebars");
 const path = require('path')
 
 
-/*configuracion*/
+/*CONFIGURACION*/
+/* establecer la carpeta views como carpeta de interfaces graficas */
 app.set("views", path.join(__dirname, "views"));
+/* configurar el motor de renderizado */
 app.engine(
   ".hbs",
   exphbs({
-    defaultLayout: "main",
-    layoutsDir: path.join(app.get("views"), "layouts"),
-    partialsDir: path.join(app.get("views"), "partials"),
-    extname: ".hbs",
+    defaultLayout: "main",/* plantilla principal */
+    layoutsDir: path.join(app.get("views"), "layouts"),/* carpeta de plantilla */
+    partialsDir: path.join(app.get("views"), "partials"),/*carpeta de porciones de codigo */
+    extname: ".hbs",/* extension de los archivos */
   })
 );
+/*seteamos el motor de renderizado */
 app.set("view engine", ".hbs");
 
-/*ruteo*/
+/*RUTEO*/
 
 app.get('/', (req, res) => {
-	res.send('hola');
+	res.render('body');
 })
 
-app.get('/perfil', (req, res) => {
-	res.render('body');
+app.get('/entrevista-dinamica', (req, res) => {
+	res.render('entrevista-dinamica');
 })
 
 
