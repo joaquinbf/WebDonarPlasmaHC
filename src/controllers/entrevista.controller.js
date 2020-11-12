467140e2293623914a443c57264d3fdf67829110
Maylen123 <molivera@etec.uba.ar>
2020-11-12 17:52:02 +0000

    Cambios

--- src/controllers/entrevista.controller.js
+++ src/controllers/entrevista.controller.js
@@ -1,7 +1,7 @@
 const entrevista_route = {}
 
 entrevista_route.render_formulario_contacto_donador = (req, res) => {
-	res.render('formulario_contacto');
+	res.render('formulario_contacto_entrevista');
 }
 
 module.exports = entrevista_route;
