915c3b52a0d0412ed45a56228b90b841bed0c555
joaquin <joaquinbf@live.com>
2020-11-20 16:38:24 -0300

    error reparado

--- src/models/Donador.js
+++ src/models/Donador.js
@@ -1,29 +1,31 @@
-const{model, Schema} = require("mongoose");
+const { model, Schema } = require("mongoose");
 
-const donador_schema = new Schema({
-    nombre:{
-        type:String,
-        required:true
+const donador_schema = new Schema(
+  {
+    nombre: {
+      type: String,
+      required: true,
     },
-    apellido:{
-        type:String,
-        required:true
+    apellido: {
+      type: String,
+      required: true,
     },
-    telefono:{
-        type:String,
-        required:true
+    telefono: {
+      type: String,
+      required: true,
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    tipo_donador:{
-        type:String,
-        required:true
+    tipo_donador: {
+      type: String,
+      required: true,
     },
-
-}, {
-    timestamps:true
-} )
-module.exports = model("Donador", donador_schema)
+  },
+  {
+    timestamps: true,
+  }
+);
+module.exports = model("Donador", donador_schema);
