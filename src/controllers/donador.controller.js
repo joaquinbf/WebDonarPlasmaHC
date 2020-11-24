257e31163249486515271a14aa8bd67939fa7ec5
Marcos <maguiremarcosd@gmai.com>
2020-11-24 17:28:22 -0300

    se agregaron chequeos de errores en agregar donador

--- src/controllers/donador.controller.js
+++ src/controllers/donador.controller.js
@@ -4,14 +4,37 @@ const Donador = require("../models/Donador");
 donador_route.agregar_donador = async (req, res) => {
   console.log(req.body);
   const { nombre, apellido, telefono, email, tipo_donador } = req.body;
-  const nuevo_donador = new Donador({
-    nombre,
-    apellido,
-    telefono,
-    email,
-    tipo_donador,
-  });
-  await nuevo_donador.save();
+  const errors = [];
+
+
+  if (!nombre) {
+    errors.push({ text: "Porfavor ingresa el nombre" });
+  }
+  if (!apellido) {
+    errors.push({ text: "Porfavor ingresar Apellido" });
+  }
+  if (!telefono) {
+    errors.push({ text: "Porfavor ingresar numero de telefono" });
+  }
+  if (!email) {
+    errors.push({ text: "Porfavor ingresar email" });
+  }
+  if (!tipo_donador) {
+    errors.push({ text: "Profavor ingresar su tipo" });
+  }
+  if (errors.length > 0) {
+    console.log("Exploto")
+  }else{
+    const nuevo_donador = new Donador({
+      nombre,
+      apellido,
+      telefono,
+      email,
+      tipo_donador,
+    });
+      await nuevo_donador.save();
+  }
+
 };
 donador_route.listar_donadores = async (req, res) => {
   const donadores = await Donador.find({}).lean();
