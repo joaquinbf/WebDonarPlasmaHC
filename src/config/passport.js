dc20d5c864fb68200d46fcf016775322b7c8fa12
joaquin <joaquinbf@live.com>
2020-11-26 16:02:11 -0300

    cambiamos funciones arrow por function

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,33 +1,38 @@
-const passport = require('passport'); 
+const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const Usuario = require ('../models/Usuario');
+const Usuario = require('../models/Usuario');
 
-passport.use(new LocalStrategy({
-  usernameField: 'user'
-}, async (user, password, done) => {
-  const usuario = await Usuario.findOne({user: user});
-  if (!user) {
-    return done(null, false, { message: 'Nombre de usuario o contraseña incorrecto.' });
-  } else {
-    const match = await usuario.comparar_password(password);
-    if(match) {
-      return done(null, user);
-    } else {
-      return done(null, false, { message: 'Nombre de usuarios o contraseña incorrecto'});
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: 'user',
+    },
+    async function (user, password, done) {
+      const usuario = await Usuario.findOne({ user: user });
+      if (!usuario) {
+        return done(null, false, {
+          message: 'Nombre de usuario o contraseña incorrecto.',
+        });
+      } else {
+        const match = await usuario.comparar_password(password);
+        if (match) {
+          return done(null, user);
+        } else {
+          return done(null, false, {
+            message: 'Nombre de usuarios o contraseña incorrecto',
+          });
+        }
+      }
     }
-  }
-}));
+  )
+);
 
- 
-
-passport.serializeUser((usuario, done) => {
+passport.serializeUser(function (usuario, done) {
   done(null, usuario.id);
 });
 
- 
-
-passport.deserializeUser((id, done) => {
-  Usuario.findById(id, (err, user) => {
+passport.deserializeUser(function (id, done) {
+  Usuario.findById(id, function (err, user) {
     done(err, user);
   });
 });
